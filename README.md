514745bbbf0a0bf90e67223bd633dfdf4d77ece8
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-20 10:44:56 +0000

    add date and time functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # work-day-scehduler
 
+## Credits
+* <a href="https://stackoverflow.com/users/3951400/e-sundin">E. Sundin</a>  https://stackoverflow.com/questions/34625764/moment-js-dynamically-update-time-in-seconds
 
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Date and Time
+var today = moment().format("dddd Do YYYY, hh:mm:ss A");
+$("#currentDay").text(today);
+//This function makes moment.js dynamically update time in seconds
+var update = function() {
+  document.getElementById("currentDay").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(update, 1000);
+
