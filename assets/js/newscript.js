64c8734a009325f9ccfbafb5e26ca89f046ade8f
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-20 23:03:26 +0000

    add newscript.js: trying to make the checkTime function shorter and more effective

---
+++ assets/js/newscript.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {// Instructs the browser to only load script file after loading the html 
+  
+  // Variable declarations
+  var today = moment().format("dddd Do YYYY, hh:mm:ss A");
+  var timeBlockEl = $(".timeBlock");
+
+  // Header date and time
+  $("#currentDay").text(today);
+  //This function makes moment.js dynamically update time every second
+  var update = function() {
+    document.getElementById("currentDay").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+  }
+  setInterval(update, 1000);
+
+  function checkTime() {
+    var currentHour = moment().format("h");
+    $(".timeBlock").each(function () {
+      var time = parseInt($(this).attr("id").split("-")[1]); //The number after the '-' character is the value assigned to time
+      
+      if (currentHour === time) {
+        timeBlockEl.children("textarea").addClass("present");
+        console.log("Y");
+      } else if (currentHour > time) {
+        timeBlockEl.children("textarea").addClass("past");
+        console.log("Current Hour: " + currentHour);
+      } else {
+        timeBlockEl.children("textarea").addClass("future");
+        console.log("Y");
+      }
+    });
+  }
+  checkTime();
+});
