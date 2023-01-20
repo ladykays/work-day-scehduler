809cc60b8a966cc5904d3d14a8fc676886c09891
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-20 17:19:36 +0000

    add checkTime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,54 @@ var update = function() {
 setInterval(update, 1000);
 
 //Timeblocks
+var hour09 = moment().hour(9);
+var hour10 = moment().hour(10);
+var hour11 = moment().hour(10);
+var hour12 = moment().hour(10);
+var hour13 = moment().hour(10);
+var hour14 = moment().hour(10);
+var hour15 = moment().hour(10);
+var hour16 = moment().hour(10);
+var hour17 = moment().hour(10);
+
+function checkTime() {
+  var currentTime = moment();
+
+  // Colorcode for hour 9
+  if (currentTime.isBetween(hour09, hour10)) {
+    $("#text-area-09").addClass("present");
+  } else if (currentTime.isAfter(hour09)) {
+    $("#text-area-09").removeClass("present");
+    $("#text-area-09").addClass("past");
+  } else {
+    $("#text-area-09").removeClass("present");
+    $("#text-area-09").removeClass("past");
+    $("#text-area-09").addClass("future");
+  }
+
+  // Colorcode for hour 10
+  if (currentTime.isBetween(hour10, hour11)) {
+    $("#text-area-10").addClass("present");
+  } else if (currentTime.isAfter(hour10)) {
+    $("#text-area-10").removeClass("present");
+    $("#text-area-10").addClass("past");
+  } else {
+    $("#text-area-10").removeClass("present");
+    $("#text-area-10").removeClass("past");
+    $("#text-area-10").addClass("future");
+  }
+  
+}
+checkTime();
+
+
+
+
+//Textarea
+var textarea = $(".text-area");
+
+
+textarea.on("click", function(e) {
+
+
+});
