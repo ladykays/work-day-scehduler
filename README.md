f04b2938d02c67b22c55f8aec696acf4ea2b569b
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-21 23:36:20 +0000

    add description, technologies used and instructions to README file. change filename from newscript.js.script.js

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
-# work-day-scehduler
+# Work Day Scehduler
+
+## Description
+This repository contains the source files for a simple calendar application. The app enables a user to save events for each hour of their work day. I began with a starter code and modified it to achieve the desired functionality. The app runs in a web browser, featuring dynamically updated HTML and CSS powered by jQuery and Moment.js library for date and time functionality.
+
+## Technologies Used
+* HTML
+* CSS
+* JavaScript
+* jQuery
+* Moment.js
+* Bootstrap
+* Font Awesome
+
+## Instructions
+* Open the application and click on the text area to add your schedule for each hour of the work day. 
+* Click the save button corresponding to a particular time to save the schedule to your local storage, so that the data will remain even after a page refresh.
+* Click the trash button corresponding to a particular time to delete the item from your schedule (Note: this does NOT delete the item from local storage if it has been saved).
+* To delete ALL saved entries in the local storage, double-click any trash button.
+
+## Live Application
+https://ladykays.github.io/work-day-scheduler
+
+## Screenshots
+![Screenshot](assets/images/screenshot.png)
 
 ## Credits
 * <a href="https://stackoverflow.com/users/3951400/e-sundin">E. Sundin</a>  
   https://stackoverflow.com/questions/34625764/moment-js-dynamically-update-time-in-seconds
 
+* https://blog.logrocket.com/localstorage-javascript-complete-guide/add description
+
+* https://momentjs.com/docs/#/get-set/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,161 +1,69 @@
-//Date and Time
-var today = moment().format("dddd Do YYYY, hh:mm:ss A");
-$("#currentDay").text(today);
-//This function makes moment.js dynamically update time in seconds
-var update = function() {
-  document.getElementById("currentDay").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-}
-setInterval(update, 1000);
+$(document).ready(function () {// Instructs the browser to only load script file after loading the html 
+  
+  // Variable declarations
+  var today = moment().format("dddd Do YYYY, hh:mm:ss A");
+ 
 
-//Timeblocks
-var hour9 = moment().hour(9)/* .minute(59) */;
-var hour10 = moment().hour(10);
-var hour11 = moment().hour(11);
-var hour12 = moment().hour(12);
-var hour13 = moment().hour(13);
-var hour14 = moment().hour(14);
-var hour15 = moment().hour(15);
-var hour16 = moment().hour(16);
-var hour17 = moment().hour(17);
-var hour18 = moment().hour(18);
-//TODO: Remove this later as this is to test if the colorcode is working properly
-var hour19 = moment().hour(19);
-var hour22 = moment().hour(22);
-var hour23 = moment().hour(23); 
-var hour24 = moment().hour(24); 
-
-
-//var timeBlockEl = $(".timeBlock");
-
-function checkTime() {
-   var currentTime = moment();
-
-  // Colorcode for hour 9
-  if (currentTime.isBetween(hour9, hour10)) {
-    $("#text-area-9").addClass("present");
-  } else if (currentTime.isAfter(hour10)) {
-    $("#text-area-9").addClass("past");
-  } else {
-    $("#text-area-9").addClass("future");
+  // Header date and time
+  $("#currentDay").text(today);
+  //This function makes moment.js dynamically update time every second
+  var update = function() {
+    document.getElementById("currentDay").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
   }
+  setInterval(update, 1000);
 
-  // Colorcode for hour 10
-  if (currentTime.isBetween(hour10, hour11)) {
-    $("#text-area-10").addClass("present");
-  } else if (currentTime.isAfter(hour11)) {
-    $("#text-area-10").addClass("past");
-  } else {
-    $("#text-area-10").addClass("future");
-  }
-
-  // Colorcode for hour 11
-  if (currentTime.isBetween(hour11, hour12)) {
-    $("#text-area-11").addClass("present");
-  } else if (currentTime.isAfter(hour12)) {
-    $("#text-area-11").addClass("past");
-  } else {
-    $("#text-area-11").addClass("future");
-  }
-
-  // Colorcode for hour 12
-  if (currentTime.isBetween(hour12, hour13)) {
-    $("#text-area-12").addClass("present");
-  } else if (currentTime.isAfter(hour13)) {
-    $("#text-area-12").addClass("past");
-  } else {
-    $("#text-area-12").addClass("future");
-  }
-
-  // Colorcode for hour 13
-  if (currentTime.isBetween(hour13, hour14)) {
-    $("#text-area-13").addClass("present");
-  } else if (currentTime.isAfter(hour14)) {
-    $("#text-area-13").addClass("past");
-  } else {
-    $("#text-area-13").addClass("future");
-  }
-
-  // Colorcode for hour 14
-  if (currentTime.isBetween(hour14, hour15)) {
-    $("#text-area-14").addClass("present");
-  } else if (currentTime.isAfter(hour15)) {
-    $("#text-area-14").addClass("past");
-  } else {
-    $("#text-area-14").addClass("future");
-  }
-
-  // Colorcode for hour 15
-  if (currentTime.isBetween(hour15, hour16)) {
-    $("#text-area-15").addClass("present");
-  } else if (currentTime.isAfter(hour16)) {
-    $("#text-area-15").addClass("past");
-  } else {
-    $("#text-area-15").addClass("future");
-  }
-
-  // Colorcode for hour 16
-  if (currentTime.isBetween(hour16, hour17)) {
-    $("#text-area-16").addClass("present");
-  } else if (currentTime.isAfter(hour17)) {
-    $("#text-area-16").addClass("past");
-  } else {
-    $("#text-area-16").addClass("future");
-  }
-
-  // Colorcode for hour 17
-  if (currentTime.isBetween(hour17, hour18)) {
-    $("#text-area-17").addClass("present");
-  } else if (currentTime.isAfter(hour18)) {
-    $("#text-area-17").addClass("past");
-  } else {
-    $("#text-area-17").addClass("future");
-  }
-
-  //TODO: Remove this later as this is to test if the colorcode is working properly
-
-  // Colorcode for hour 22
-  if (currentTime.from(hour22, hour23)) {
-    $("#text-area-22").addClass("present");
-  } else if (currentTime.isAfter(hour23)) {
-    $("#text-area-22").addClass("past");
-  } else {
-    $("#text-area-22").addClass("future");
-  }
-
-  // Colorcode for hour 23
-  if (currentTime.isBetween(hour23, hour24)) {
-    $("#text-area-23").addClass("present");
-  } else if (currentTime.isAfter(hour24)) {
-    $("#text-area-23").addClass("past");
-  } else {
-    $("#text-area-23").addClass("future"); 
-  }
-
-  /*
-  $("timeBlock").each(function () {
-    var time = parseInt($(this).attr("id").split("-")[1]); //The number after the '-' character is the value assigned to time
-    
-    if (currentHour === time) {
-      timeBlockEl.children("text-area").addClass("present");
+  // Function to check time and assign color
+  function checkTime() {
+    var currentHour = moment().hour();
+    $(".timeBlock").each(function () {
+      var time = parseInt($(this).attr("id").split("-")[1]); //The number after the '-' character is the value assigned to time
       
-    } else if (currentHour > time) {
-      timeBlockEl.children("text-area").addClass("past");
-      console.log("Current Hour: " + currentHour);
-    } else {
-      timeBlockEl.children("text-area").addClass("future");
-    }
-  }); */
-} 
-checkTime();
+      if (time < currentHour) {
+        $(this).children("textarea").addClass("past");
+        console.log("Past");
+      } else if (time === currentHour) {
+        $(this).children("textarea").addClass("present");
+        console.log("Current Hour: " + currentHour);
+      } else {
+        $(this).children("textarea").addClass("future");
+        console.log(time);
+      }
+      //console.log(time);
+    });
+    
+  }
+  checkTime();
 
+  //Event handlers
 
+  // Function to save schedule
+  $(".saveBtn").click(function(event) {
+    var schedule = $(this).siblings(".text-area").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, schedule);
+  });
+    
+ // Button function to clear text in textarea
+  $(".clearBtn").on("click", function () {
+    $(this).siblings(".text-area").val("");
+  });
 
+  // Button function to clear the entire local storage contents
+  $(".clearBtn").on("dblclick", function() {
+    $("textarea").val("");
+    localStorage.clear();
+  }); 
+  
 
-//Textarea
-var textarea = $(".text-area");
-
-
-textarea.on("click", function(e) {
-
+  // Retain items on page reload
+  $("#timeBlock-9 .text-area").val(localStorage.getItem("9"));
+  $("#timeBlock-10 .text-area").val(localStorage.getItem("10"));
+  $("#timeBlock-11 .text-area").val(localStorage.getItem("11"));
+  $("#timeBlock-12 .text-area").val(localStorage.getItem("12"));
+  $("#timeBlock-13 .text-area").val(localStorage.getItem("13"));
+  $("#timeBlock-14 .text-area").val(localStorage.getItem("14"));
+  $("#timeBlock-15 .text-area").val(localStorage.getItem("15"));
+  $("#timeBlock-16 .text-area").val(localStorage.getItem("16"));
+  $("#timeBlock-17 .text-area").val(localStorage.getItem("17"));
 
 });
