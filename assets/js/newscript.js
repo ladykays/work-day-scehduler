e3c510bd6cbe6a2038c17d6d8c8a2de40f369e14
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-01-21 22:11:35 +0000

    add clear button function

--- assets/js/newscript.js
+++ assets/js/newscript.js
@@ -2,7 +2,7 @@ $(document).ready(function () {// Instructs the browser to only load script file
   
   // Variable declarations
   var today = moment().format("dddd Do YYYY, hh:mm:ss A");
-  var timeBlockEl = $(".timeBlock");
+  //var timeBlockEl = $(".timeBlock");
 
   // Header date and time
   $("#currentDay").text(today);
@@ -34,13 +34,49 @@ $(document).ready(function () {// Instructs the browser to only load script file
   checkTime();
 
   //Event handlers
-  $(".saveBtn").on("click", function () {
+  // Function to save schedule
+  $(".saveBtn").click(function(event) {
+    //event.preventDefault();
     var schedule = $(this).siblings(".text-area").val();
     var time = $(this).parent().attr("id").split("-")[1];
     localStorage.setItem(time, schedule);
-    console.log(localStorage);
+  });
+    
+
+ // Button function to clear text in textarea
+  $(".clearBtn").on("click", function () {
+    $(this).siblings(".text-area").val("");
   });
 
+  // Button function to clear the entire local storage contents
+  $(".clearBtn").on("dblclick", function() {
+    $("textarea").val("");
+    localStorage.clear();
+  }); 
+  
   // Retain items on page reload
+  $("#timeBlock-9 .text-area").val(localStorage.getItem("9"));
+  $("#timeBlock-10 .text-area").val(localStorage.getItem("10"));
+  $("#timeBlock-11 .text-area").val(localStorage.getItem("11"));
+  $("#timeBlock-12 .text-area").val(localStorage.getItem("12"));
+  $("#timeBlock-13 .text-area").val(localStorage.getItem("13"));
+  $("#timeBlock-14 .text-area").val(localStorage.getItem("14"));
+  $("#timeBlock-15 .text-area").val(localStorage.getItem("15"));
+  $("#timeBlock-16 .text-area").val(localStorage.getItem("16"));
+  $("#timeBlock-17 .text-area").val(localStorage.getItem("17"));
 
 });
+
+
+ /*  // Button function to clear local storage and clear contents
+  $(".clearBtn").click(function () {
+    $(this).siblings(".text-area").val("");
+    localStorage.removeItem();
+  }); */
+  
+
+  /* // Button function to clear local storage and clear contents
+  $(".clearBtn").click(function () {
+    $(".text-area").val("");
+    localStorage.removeItem();
+  }); */
